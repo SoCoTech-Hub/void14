9a8d2616deb44937e36e4511b3feffe1f6137f9d
Ben Henning <benhenning10@gmail.com>
2024-07-04 11:17:29 +0200

    remove signInAction

--- apps/api/admin-preset-api/lib/actions/users.ts
+++ apps/api/admin-preset-api/lib/actions/users.ts
@@ -1,104 +1,15 @@
 "use server";
 
 import { revalidatePath } from "next/cache";
-import { redirect } from "next/navigation";
 import { eq } from "drizzle-orm";
-import { generateId } from "lucia";
-import { Argon2id } from "oslo/password";
 
-import { lucia, validateRequest } from "@soco/auth/lucia";
+import { genericError, getUserAuth } from "@soco/auth/utils";
 
-import {
-  genericError,
-  getUserAuth,
-  setAuthCookie,
-  validateAuthFormData,
-} from "@soco/auth/utils";
 import { db } from "../db/index";
 import { updateUserSchema, users } from "../db/schema/auth";
 
 interface ActionResult {
   error: string;
-}
-
-export async function signInAction(
-  _: ActionResult,
-  formData: FormData,
-): Promise<ActionResult> {
-  const { data, error } = validateAuthFormData(formData);
-  if (error !== null) return { error };
-
-  try {
-    const [existingUser] = await db
-      .select()
-      .from(users)
-      .where(eq(users.email, data.email.toLowerCase()));
-    if (!existingUser) {
-      return {
-        error: "Incorrect username or password",
-      };
-    }
-
-    const validPassword = await new Argon2id().verify(
-      existingUser.hashedPassword,
-      data.password,
-    );
-    if (!validPassword) {
-      return {
-        error: "Incorrect username or password",
-      };
-    }
-
-    const session = await lucia.createSession(existingUser.id, {});
-    const sessionCookie = lucia.createSessionCookie(session.id);
-    setAuthCookie(sessionCookie);
-
-    return redirect("/dashboard");
-  } catch (e) {
-    return genericError;
-  }
-}
-
-export async function signUpAction(
-  _: ActionResult,
-  formData: FormData,
-): Promise<ActionResult> {
-  const { data, error } = validateAuthFormData(formData);
-
-  if (error !== null) return { error };
-
-  const hashedPassword = await new Argon2id().hash(data.password);
-  const userId = generateId(15);
-
-  try {
-    await db.insert(users).values({
-      id: userId,
-      email: data.email,
-      hashedPassword,
-    });
-  } catch (e) {
-    return genericError;
-  }
-
-  const session = await lucia.createSession(userId, {});
-  const sessionCookie = lucia.createSessionCookie(session.id);
-  setAuthCookie(sessionCookie);
-  return redirect("/dashboard");
-}
-
-export async function signOutAction(): Promise<ActionResult> {
-  const { session } = await validateRequest();
-  if (!session) {
-    return {
-      error: "Unauthorized",
-    };
-  }
-
-  await lucia.invalidateSession(session.id);
-
-  const sessionCookie = lucia.createBlankSessionCookie();
-  setAuthCookie(sessionCookie);
-  redirect("/sign-in");
 }
 
 export async function updateUser(
