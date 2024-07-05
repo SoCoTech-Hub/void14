c0c1d1ae462286ccdb5debaecf7391fe67ac5173
Ben Henning <benhenning10@gmail.com>
2024-07-05 13:07:06 +0200

    create db setup separate from api setup

---
+++ packages/db/user-db/src/schema/userDevices.ts
@@ -0,0 +1,59 @@
+import type { z } from "zod";
+import { sql } from "drizzle-orm";
+import { pgTable, timestamp, uniqueIndex, varchar } from "drizzle-orm/pg-core";
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
+
+import { nanoid, timestamps } from "@soco/utils";
+
+export const userDevices = pgTable(
+  "user_devices",
+  {
+    organizationId: varchar("organization_id", { length: 191 }).notNull(),
+    id: varchar("id", { length: 191 })
+      .primaryKey()
+      .$defaultFn(() => nanoid()),
+    appId: varchar("app_id", { length: 256 }),
+    model: varchar("model", { length: 256 }),
+    name: varchar("name", { length: 256 }),
+    platform: varchar("platform", { length: 256 }),
+    pushId: varchar("push_id", { length: 256 }),
+    uuid: varchar("uuid", { length: 256 }),
+    version: varchar("version", { length: 256 }),
+    userId: varchar("user_id", { length: 256 }).notNull(),
+
+    createdAt: timestamp("created_at")
+      .notNull()
+      .default(sql`now()`),
+    updatedAt: timestamp("updated_at")
+      .notNull()
+      .default(sql`now()`),
+  },
+  (userDevices) => {
+    return {
+      appIdIndex: uniqueIndex("app_id_idx").on(userDevices.appId),
+    };
+  },
+);
+
+// Schema for userDevices - used to validate API requests
+const baseSchema = createSelectSchema(userDevices).omit(timestamps);
+
+export const insertUserDeviceSchema =
+  createInsertSchema(userDevices).omit(timestamps);
+export const insertUserDeviceParams = baseSchema.extend({}).omit({
+  id: true,
+  userId: true,
+});
+
+export const updateUserDeviceSchema = baseSchema;
+export const updateUserDeviceParams = baseSchema.extend({}).omit({
+  userId: true,
+});
+export const userDeviceIdSchema = baseSchema.pick({ id: true });
+
+// Types for userDevices - used to type API request params and within Components
+export type UserDevice = typeof userDevices.$inferSelect;
+export type NewUserDevice = z.infer<typeof insertUserDeviceSchema>;
+export type NewUserDeviceParams = z.infer<typeof insertUserDeviceParams>;
+export type UpdateUserDeviceParams = z.infer<typeof updateUserDeviceParams>;
+export type UserDeviceId = z.infer<typeof userDeviceIdSchema>["id"];
---
+++ packages/db/user-db/src/schema/userPasswordHistories.ts
@@ -0,0 +1,54 @@
+import type { z } from "zod";
+import { sql } from "drizzle-orm";
+import { pgTable, timestamp, varchar } from "drizzle-orm/pg-core";
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
+
+import { nanoid, timestamps } from "@soco/utils";
+
+export const userPasswordHistories = pgTable("user_password_histories", {
+  organizationId: varchar("organization_id", { length: 191 }).notNull(),
+  id: varchar("id", { length: 191 })
+    .primaryKey()
+    .$defaultFn(() => nanoid()),
+  hash: varchar("hash", { length: 256 }),
+  userId: varchar("user_id", { length: 256 }).notNull(),
+
+  createdAt: timestamp("created_at")
+    .notNull()
+    .default(sql`now()`),
+  updatedAt: timestamp("updated_at")
+    .notNull()
+    .default(sql`now()`),
+});
+
+// Schema for userPasswordHistories - used to validate API requests
+const baseSchema = createSelectSchema(userPasswordHistories).omit(timestamps);
+
+export const insertUserPasswordHistorySchema = createInsertSchema(
+  userPasswordHistories,
+).omit(timestamps);
+export const insertUserPasswordHistoryParams = baseSchema.extend({}).omit({
+  id: true,
+  userId: true,
+});
+
+export const updateUserPasswordHistorySchema = baseSchema;
+export const updateUserPasswordHistoryParams = baseSchema.extend({}).omit({
+  userId: true,
+});
+export const userPasswordHistoryIdSchema = baseSchema.pick({ id: true });
+
+// Types for userPasswordHistories - used to type API request params and within Components
+export type UserPasswordHistory = typeof userPasswordHistories.$inferSelect;
+export type NewUserPasswordHistory = z.infer<
+  typeof insertUserPasswordHistorySchema
+>;
+export type NewUserPasswordHistoryParams = z.infer<
+  typeof insertUserPasswordHistoryParams
+>;
+export type UpdateUserPasswordHistoryParams = z.infer<
+  typeof updateUserPasswordHistoryParams
+>;
+export type UserPasswordHistoryId = z.infer<
+  typeof userPasswordHistoryIdSchema
+>["id"];
